3bf61df29512f411c0e694be0bec428aae5367a7
VINICIUS FAVORITO RODRIGUES <vfavorito7@gmail.com>
2021-09-28 21:39:04 -0300

    Aula 2.2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 const { request, response } = require('express');
 const express = require('express');
+const{v4:uuidv4} = require("uuid")
 
 const app = express();
+
+app.use(express.json())
+
+const costumers = []
+
+/**
+ * cpf  - string
+ * name - sttring
+ * id - uuid
+ * statement - []
+ */
+app.post("/conta",(request,response)=>{
+  const {cpf,name} = request.body;
+  const id = uuidv4();
+
+  costumers.push({
+    cpf,
+    name,
+    id,
+    statement:[]
+  })
+  return response.status(201).send();
+})
 
 app.listen(3333)
