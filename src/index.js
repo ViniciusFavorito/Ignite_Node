ee5305fb818196140dc574414b7bf04768202d76
VINICIUS FAVORITO RODRIGUES <vfavorito7@gmail.com>
2021-09-28 21:05:20 -0300

    Aula 2.3

--- src/index.js
+++ src/index.js
@@ -4,10 +4,33 @@ const express = require('express');
 const app = express();
 
 
+/**
+ * get - buscar informação
+ * post - inserir informação no servidor
+ * put - alterar informação do servidor
+ * patch - alterar informação especifica
+ * delete - deletar informação do servidor
+ */
 
-app.get("/",(request,response)=>{
+app.get("/courses",(request,response)=>{
   //return response.send("hello world")
-  return response.json({message:"Hello World Ignite"})
+  return response.json(["Curso 1", "Curso 2","Curso 3"])
+})
+
+app.post("/courses",(request,response)=>{
+  return response.json(["Curso 1", "Curso 2","Curso 3","Curso 4"])
+})
+
+app.put("/courses/:id",(request,response)=>{
+  return response.json(["Curso 6", "Curso 2","Curso 3","Curso 4"])
+})
+
+app.patch("/courses/:id",(request,response)=>{
+  return response.json(["Curso 6", "Curso 7","Curso 3","Curso 4"])
+})
+
+app.delete("/courses/:id",(request,response)=>{
+  return response.json(["Curso 6", "Curso 7","Curso 4"])
 })
 
 app.listen(3333)
